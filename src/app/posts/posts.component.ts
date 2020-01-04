b567c9e096486b648542b10e228f57d1c38fd058
Manu M <manumohan.cvav@gmail.com>
2020-01-05 00:24:13 +0530

    Updated notes

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+
+  posts: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts')
+  }
+
+}
